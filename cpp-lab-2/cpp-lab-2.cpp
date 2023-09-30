0c6f239c91529ea5a41e231227708b60b50620a6
w0nderfuu1 <pokonovalov@edu.hse.ru>
2023-09-30 13:10:19 +0300

    Добавьте файлы проекта.

---
+++ cpp-lab-2/cpp-lab-2.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+long double fuctorial_2(long int x) {
+	long double result = 1;
+
+	for (int i = x; i > 0; i -= 2) {
+		result *= i;
+	}
+	
+	return result;
+}
+
+long double stepen(long double bs, int pow) {
+	long double result = 1;
+
+	for (int i = 0; i < pow; i++) {
+		result *= bs;
+	}
+
+	return result;
+}
+
+long double getT(long double x) {
+	long double znam = 0;
+
+	for (int i = 0; i < 10; i++) {
+		long double s_znam = fuctorial_2(i * 2);
+		long double s_chis = stepen(x, 2 * i);
+		znam += s_chis / s_znam;
+	}
+	long double chis = 0;
+
+	for (int i = 0; i < 10; i++) {
+		long double s_znam = fuctorial_2(i * 2 + 1);
+		long double s_chis = stepen(x, 2 * i + 1);
+		chis += s_chis / s_znam;
+	}
+
+	return chis / znam;
+}
+
+long double getRes(long double y) {
+	long double znam = 6 - getT(y * y - 1);
+	long double chis = 7 * getT(0.25) + 2 * getT(1 + y);
+	
+	return chis / znam;
+}
+
+int main() {
+	long double y;
+	printf("Enter y:");
+	scanf("%Lf", &y);
+
+	printf("Answer: %.10Lf", getRes(y));
+}
